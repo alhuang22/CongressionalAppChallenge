b6b1abb94161c36c893c1ee7f32a0aad27fa2d04
Alex Huang <alexxh0098@gmail.com>
2021-06-27 10:53:39 -0700

    add button transfer objective C functions

---
+++ CongressionalAppChallenge/College_VC.swift
@@ -0,0 +1,8 @@
+//
+//  College_VC.swift
+//  CongressionalAppChallenge
+//
+//  Created by Alex Huang on 6/26/21.
+//
+
+import Foundation
---
+++ CongressionalAppChallenge/Scholarship_VC.swift
@@ -0,0 +1,8 @@
+//
+//  Scholarship_VC.swift
+//  CongressionalAppChallenge
+//
+//  Created by Alex Huang on 6/26/21.
+//
+
+import Foundation
