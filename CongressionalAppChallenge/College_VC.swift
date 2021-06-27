79549c44a4354222a3e2131a098889c66b7c81bb
Jonathan-Chi <86543981+Jonathan-Chi@users.noreply.github.com>
2021-06-26 22:08:56 -0700

    Add files via upload

---
+++ CongressionalAppChallenge/College_VC.swift
@@ -0,0 +1,23 @@
+//
+//  College_VC.swift
+//  test101
+//
+//  Created by Jonathan Chi on 6/26/21.
+//
+
+import Foundation
+
+import UIKit
+
+
+class College_VC : UIViewController {
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.systemBlue
+        
+    }
+    
+    
+}
+
